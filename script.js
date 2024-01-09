ff0dbdc79ac2ad317b098c713908bfd61b883057
Nicolas de Almeida MAvroucakis <nicolas.mavroucakis@gmail.com>
2024-01-09 16:21:00 -0300

    acabando

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarTexto() {
+    document.getElementsByClassName("project").style.backgroundColor = "lightblue";
+    document.querySelector(".conteudo").style.display = "block";
+  }
+  function esconderTexto() {
+    document.getElementsByClassName("project").style.backgroundColor = "";
+    document.querySelector(".conteudo").style.display = "none";
+  }

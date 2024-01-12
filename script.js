1f169e49c5cf29e765275b1f5883e20e71a0ae97
Nicolas de Almeida MAvroucakis <nicolas.mavroucakis@gmail.com>
2024-01-11 21:37:41 -0300

    acabou

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function mostrarTexto() {
-    document.getElementsByClassName("project").style.backgroundColor = "lightblue";
-    document.querySelector(".conteudo").style.display = "block";
-  }
-  function esconderTexto() {
-    document.getElementsByClassName("project").style.backgroundColor = "";
-    document.querySelector(".conteudo").style.display = "none";
-  }
+const textElement = document.querySelector('.text')
+
+    setInterval(() => {
+      textElement.style.animation = 'none'
+      void textElement.offsetWidth
+      textElement.textContent = textElement.textContent === 'A Web Developer' ? 'A Software Engineer' : 'A Web Developer'
+      textElement.style.animation = 'typing 2s steps(20, end), cursor .4s step-end infinite alternate'
+    }, 4000)
+    
+const changeTextElements = document.querySelectorAll('.change-text')
+
+setInterval(() => {
+  changeTextElements.forEach(element => {
+    element.style.animation = 'none'
+    void element.offsetWidth
+    element.textContent = element.textContent === 'de Almeida' ? 'Mavroucakis' : 'de Almeida'
+    element.style.animation = 'typing 2s steps(20, end), cursor .4s step-end infinite alternate'
+  })
+}, 4000)
+
+const hamburger = document.querySelector(".hamburger")
+const nav = document.querySelector(".nav")
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"))
+
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav-list a');
+
+    function hideNavList() {
+        nav.classList.remove('active');
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', hideNavList);
+    });
+});
